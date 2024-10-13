238b73dd5f29d6ada6a07e942958ee398f36ef9d
tryconnection <xaviza11@gmail.com>
2024-10-13 22:22:45 +0200

    add unique to user email

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 from dotenv import load_dotenv
 import os
 
@@ -8,3 +8,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
 db = client['piano_app'] 
+
+
+users_collection = db.users
+users_collection.create_index([("email", ASCENDING)], unique=True)
